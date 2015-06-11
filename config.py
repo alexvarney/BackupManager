446bda479fb7334ffcf457a43e83b36211f839bf
Alex Varney <alex@alexvarney.com>
2015-06-11 09:44:17 -0400

    fixed config file defaults

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ __author__ = 'alex'
 
 #Paths require a trailing slash in order to archive properly
 
-storage_location = "/Users/alex/Documents/Programming/BackupManager Files/"
-directory_to_backup = "/Users/alex/Documents/BM News/"
-database_location = "/Users/alex/PycharmProjects/BackupManager/files.sqlite"
+storage_location = "/directory/"
+directory_to_backup = "/directory/"
+database_location = "/directory/files.sqlite"
 app_username = "user"
 app_password = "password"
 app_port = 8081
